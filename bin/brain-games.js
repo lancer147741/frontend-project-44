268c94e265fc93030e849e1eccb3fb9acdd53cad
lancer147741 <vip.wdwww@gmail.com>
2023-10-16 02:20:29 +0300

    step 2

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
-function brainGames() {
-    console.log("node bin/brain-games.js")
-    console.log("Welcome to the Brain Games!")
-
-}
-brainGames()
+console.log("Welcome to the Brain Games!");
+//fw
