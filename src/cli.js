b4e4f1a8b16937e70b0a89ef3504ad99713ead86
lancer147741 <vip.wdwww@gmail.com>
2023-10-16 02:32:54 +0300

    step 3

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import * as readlineSync from 'readline-sync';
+
+const greetUser = () => {
+    const userName = readlineSync.question('May I have your name?');
+    console.log(`${'Hello,'} ${userName}${'!'}`);
+};
+export default greetUser;
