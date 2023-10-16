58debb04e1fa0a8aa2defe2ef00c5dae0f199f23
lancer147741 <vip.wdwww@gmail.com>
2023-10-16 04:48:02 +0300

    add realization brain-even.js

---
+++ src/games/even.js
@@ -0,0 +1,18 @@
+import startGame from '../index.js';
+import getRandomNumbers from '../randomNumbers.js';
+
+const isEven = (number) => number % 2 === 0;
+
+const playEvenGame = () => {
+    const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+    const generateRound = () => {
+        const question = String(getRandomNumbers());
+        const answer = isEven(question) ? 'yes' : 'no';
+        return [question, answer];
+    };
+
+    startGame(rulesOfGame, generateRound);
+};
+
+export default playEvenGame;
