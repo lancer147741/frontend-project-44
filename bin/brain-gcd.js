d491b57830dd52bec303d4d2c67e04bddef1b95f
lancer147741 <vip.wdwww@gmail.com>
2023-10-16 04:54:51 +0300

    add realization brain-gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import playGCDGame from '../src/games/gcd.js';
+
+startGCDGame();
---
+++ src/games/gcd.js
@@ -0,0 +1,23 @@
+import startGame from '../index.js';
+import getRandomNumbers from '../getRandomNumber.js';
+
+const calculateGCD = (num1, num2) => {
+    if (num2 === 0) return num1;
+    return calculateGCD(num2, num1 % num2);
+}
+
+const playGCDGame = () => {
+    const rulesOfGame = 'Find the greatest common divisor of given numbers.';
+
+    const gameRound = () => {
+        const firstNumber = getRandomNumbers(0, 20);
+        const secondNumber = getRandomNumbers(0, 20);
+
+        const question = `${firstNumber} ${secondNumber}`;
+        const correctAnswer = calculateGCD(firstNumber, secondNumber).toString();
+
+        return [question, correctAnswer];
+    };
+
+    startGame(rulesOfGame, gameRound);
+};
