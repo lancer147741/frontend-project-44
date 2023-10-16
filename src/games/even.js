9686a777ce3aa29e98f87cf0ea3aef04565858fe
lancer147741 <vip.wdwww@gmail.com>
2023-10-16 05:40:58 +0300

    fix problems

--- src/games/even.js
+++ src/games/even.js
@@ -1,34 +1,18 @@
 import startGame from '../index.js';
 import getRandomNumbers from '../randomNumbers.js';
 
-const getProgression = (stepProgression, randomHiddenElem, startNumber) => {
-    const hiddenElement = '..';
-    const createdArray = [];
-    const arrayLength = 10;
-    createdArray[0] = startNumber;
+const isEven = (number) => number % 2 === 0;
 
-    for (let i = 1; i < arrayLength; i += 1) {
-        createdArray[i] = createdArray[i - 1] + stepProgression;
-    }
-    const hiddenNumber = String(createdArray.splice(randomHiddenElem, 1, hiddenElement));
-    const peogressionOfNumber = createdArray.join(' ');
+const playEvenGame = () => {
+    const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-    return [peogressionOfNumber, hiddenNumber];
-};
-
-const playBrainProgression = () => {
-    const rulesOfGames = 'What number is missing in the progression?';
-
-    const generatedRound = () => {
-        const randomHiddenElem = getRandomNumbers(0, 9);
-        const startNum = getRandomNumbers(0, 50);
-        const step = getRandomNumbers(0, 5);
-
-        const [question, answer] = getProgression(step, randomHiddenElem, startNum);
+    const generateRound = () => {
+        const question = String(getRandomNumbers());
+        const answer = isEven(question) ? 'yes' : 'no';
         return [question, answer];
     };
 
-    startGame(rulesOfGames, generatedRound);
+    startGame(rulesOfGame, generateRound)
 };
 
-export default playBrainProgression;
+export default playEvenGame;
