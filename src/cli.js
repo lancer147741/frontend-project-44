dadefae896eb0526b64d7df7c4966fcfec36976d
lancer147741 <vip.wdwww@gmail.com>
2023-10-16 05:20:25 +0300

    fix problems

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,10 @@
-import * as readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 
 const greetUser = () => {
-    const userName = readlineSync.question('May I have your name?');
-    console.log(`${'Hello,'} ${userName}${'!'}`);
+    console.log('Welcome to the Brain Games!');
+
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
 };
+
 export default greetUser;
